1c202d83eda58c27874f1af3382caa0fbdd00e42
Pythagoras <36764405+Phytagoras@users.noreply.github.com>
2022-01-28 09:40:32 +0100

    addedAllComands

--- src/main/java/my_project/control/GameServerController.java
+++ src/main/java/my_project/control/GameServerController.java
@@ -5,7 +5,7 @@ import my_project.model.ConnectedEntity;
 
 import java.util.ArrayList;
 
-public class GameServerController extends Server {
+public class GameServerController extends Server{
     private final GameController gameController;
     private final ArrayList<ConnectedEntity> players;
     int id = 0;
@@ -25,43 +25,126 @@ public class GameServerController extends Server {
     @Override
     public void processMessage(String pClientIP, int pClientPort, String pMessage){
         ConnectedEntity currentConnectedEntity = null;
-        for (int i = 0; i < players.size(); i++){
-            if (players.get(i).getIp().equals(pClientIP)){
+        for(int i = 0; i < players.size(); i++){
+            if(players.get(i).getIp().equals(pClientIP)){
                 currentConnectedEntity = players.get(i);
                 break;
             }
         }
-        if (currentConnectedEntity == null) return;              //sollte NIE vorkommen
+        if(currentConnectedEntity == null) return;              //sollte NIE vorkommen
         String[] commandFragments = pMessage.split(" ");
-        if (commandFragments.length > 1){
+        if(commandFragments.length > 1){
             if(handleCommand(pClientIP, pClientPort, commandFragments, currentConnectedEntity)){
                 //erfolgreich
-            }else {
+            }else{
                 //nicht erfolgreich
             }
-        } else if (commandFragments[0].equalsIgnoreCase("help")){
-            switch (currentConnectedEntity.getHelpCounter()) {
+        }else if(commandFragments[0].equalsIgnoreCase("help")){
+            switch(currentConnectedEntity.getHelpCounter()){                              //Help Anweisungen fehlen noch
                 default -> send(pClientIP, pClientPort, "HIER HILFE...NIMM");
             }
 
-        } else {
+        }else{
             send(pClientIP, pClientPort, "Du kannst jederzeit nach Hilfe fragen!");
         }
     }
 
     private boolean handleCommand(String pClientIP, int pClientPort, String[] commandFragments, ConnectedEntity currentConnectedEntity){
-        if (!currentConnectedEntity.isInit()){
-            if (commandFragments.length == 2){
-                if (commandFragments[0].equalsIgnoreCase("joinGame")){
-                    if (!commandFragments[1].isEmpty()){
-                        currentConnectedEntity.setInit(true);
-                        currentConnectedEntity.setName(commandFragments[1]);
-                        gameController.newPlayer(currentConnectedEntity.getId(), currentConnectedEntity.getName());
+        if(!currentConnectedEntity.isInit()){
+            if(commandFragments.length == 2){
+                if(commandFragments[0].equalsIgnoreCase("joinGame") && !commandFragments[1].isEmpty()){
+                    currentConnectedEntity.setInit(true);
+                    currentConnectedEntity.setName(commandFragments[1]);
+                    gameController.newPlayer(currentConnectedEntity.getId(), currentConnectedEntity.getName());
+                    return true;
+
+                }
+            }
+        }else{
+            //andere CommandsUeberpruefen
+            if(commandFragments.length == 2){
+                if(commandFragments[0].equalsIgnoreCase("move") && !commandFragments[1].isEmpty()){
+                    boolean found = false;
+                    for(ConnectedEntity connectedEntity : players){
+                        if(connectedEntity.getName().equals(commandFragments[1])){  //wird fuer jeden Player mit dem Namen ausgefuehrt
+                            gameController.movePlayer(connectedEntity.getId());
+                            found = true;
+                        }
+                    }
+                    if(found) return true;
+                    else{
+                        send(pClientIP, pClientPort, "Spieler existiert nicht!");
+                        return false;
+                    }
+                }
+            }else if(commandFragments.length == 3){
+                if(commandFragments[0].equalsIgnoreCase("setSpeed") && !commandFragments[1].isEmpty() && !commandFragments[2].isEmpty()){
+                    boolean found = false;
+                    double multiplier;
+                    try{
+                        multiplier = Double.parseDouble(commandFragments[1]);
+                    }catch(Exception e){
+                        this.send(pClientIP, pClientPort, "Multiplikator muss eine Kommazahl sein!");
+                        return false;
+                    }
+                    //soll der Multiplier noch begrenzt werden?
+                    for(ConnectedEntity connectedEntity : players){
+                        if(connectedEntity.getName().equals(commandFragments[2])){  //wird fuer jeden Player mit dem Namen ausgefuehrt
+                            gameController.changeSpeed(currentConnectedEntity.getId(), multiplier);
+                            found = true;
+                        }
+                    }
+                    if(found) return true;
+                    else{
+                        send(pClientIP, pClientPort, "Spieler existiert nicht!");
+                        return false;
+                    }
+                }else if(commandFragments[0].equalsIgnoreCase("spawn") && !commandFragments[1].isEmpty() && !commandFragments[2].isEmpty()){
+                    boolean found = false;
+                    int obj;
+                    switch(commandFragments[1]){
+                        case ("Banana") -> obj = 0;
+                        case ("Portal") -> obj = 1;
+                        default -> {
+                            send(pClientIP, pClientPort, "Objekt gibt es nicht...");
+                            return false;
+                        }
+                    }
+                    for(ConnectedEntity connectedEntity : players){
+                        if(connectedEntity.getName().equals(commandFragments[2])){  //wird fuer jeden Player mit dem Namen ausgefuehrt
+                            gameController.spawnObject(obj, currentConnectedEntity.getId());
+                            found = true;
+                        }
+                    }
+                    if(found) return true;
+                    else{
+                        send(pClientIP, pClientPort, "Spieler existiert nicht!");
+                        return false;
+                    }
+                }else if(commandFragments[0].equalsIgnoreCase("destroy") && !commandFragments[1].isEmpty() && !commandFragments[2].isEmpty()){
+                    boolean found = false;
+                    int obj;
+                    switch(commandFragments[1]){
+                        case ("Banana") -> obj = 0;
+                        case ("Portal") -> obj = 1;
+                        default -> {
+                            send(pClientIP, pClientPort, "Objekt gibt es nicht...");
+                            return false;
+                        }
+                    }
+                    for(ConnectedEntity connectedEntity : players){
+                        if(connectedEntity.getName().equals(commandFragments[2])){  //wird fuer jeden Player mit dem Namen ausgefuehrt
+                            gameController.destroyObject(obj, currentConnectedEntity.getId());
+                            found = true;
+                        }
+                    }
+                    if(found) return true;
+                    else{
+                        send(pClientIP, pClientPort, "Spieler existiert nicht!");
+                        return false;
                     }
                 }
             }
-        }else {
-            //andere CommandsUeberpruefen
         }
 
         return false;
